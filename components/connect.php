2c24f91155235fad4dd6bfb68e9b1c83b61c7c5d
Janinyuh <74756627+Janinyuh@users.noreply.github.com>
2022-07-08 10:33:45 +0800

    yeah

--- components/connect.php
+++ components/connect.php
@@ -5,12 +5,12 @@
 //$user_password = '';
 
 $host = 'remotemysql.com';
-$db_name = 'g1gIWGjJsM';
-$user_name = 'g1gIWGjJsM';
-$user_password = 'o92qJla1Ib';
+$db = 'g1gIWGjJsM';
+$user = 'g1gIWGjJsM';
+$pass = 'o92qJla1Ib';
 $charset = 'utf8mb4';
 
 
-$conn = new PDO($host,$db_name, $user_name, $user_password);
+$conn = new PDO($host,$db, $user, $pass);
 
 ?>
