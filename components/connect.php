425970998f9acee9975a2353c201548c7bed838e
Janinyuh <74756627+Janinyuh@users.noreply.github.com>
2022-07-08 10:27:35 +0800

    Update connect.php

--- components/connect.php
+++ components/connect.php
@@ -1,9 +1,16 @@
 <?php
 
-$db_name = 'mysql:host=localhost;dbname=shop_db';
-$user_name = 'root';
-$user_password = '';
+//$db_name = 'mysql:host=localhost;dbname=shop_db';
+//$user_name = 'root';
+//$user_password = '';
 
-$conn = new PDO($db_name, $user_name, $user_password);
+$host = 'remotemysql.com';
+$db_name = 'g1gIWGjJsM';
+$user_name = 'g1gIWGjJsM';
+$user_password = 'o92qJla1Ib';
+$charset = 'utf8mb4';
+
+
+$conn = new PDO($host,$db_name, $user_name, $user_password);
 
 ?>
