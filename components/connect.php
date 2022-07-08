cb8ded27ffbd60b0391cf77aeac9745a74918f31
Janinyuh <74756627+Janinyuh@users.noreply.github.com>
2022-07-08 12:27:38 +0800

    h

--- components/connect.php
+++ components/connect.php
@@ -1,8 +1,5 @@
 <?php
 
-//$db_name = 'mysql:host=localhost;dbname=shop_db';
-//$user_name = 'root';
-//$user_password = '';
 
 $host = 'remotemysql.com';
 $db = 'g1gIWGjJsM';
@@ -11,6 +8,10 @@ $pass = 'o92qJla1Ib';
 $charset = 'utf8mb4';
 
 
-$conn = new PDO($host,$db, $user, $pass);
+//$conn = new PDO($host,$db, $user, $pass);
+$conn = new PDO('mysql:host=' . $host .';port=3306;dbname='.$db,
+    $user,
+    $pass,
+    array(PDO::ATTR_PERSISTENT => true));
 
 ?>
